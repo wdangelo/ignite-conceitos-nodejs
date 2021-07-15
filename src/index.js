ba29ff68fc208eb30ee947463d82790f00ba6705
Willian de Angelo <wdangelo1983@gmail.com>
2021-07-15 15:11:55 -0300

    Criado rota de criação de usuários

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos:[]
+
+  })
+
+  return response.status(200).json(users)
+  
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
